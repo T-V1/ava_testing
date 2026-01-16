a69bb46fae3be27ab402df481b7304f3bce128e8
T-V1 <teja.vajapeyazula@ascendion.com>
2026-01-16 12:34:07 +0530

    Update Calculator.java

--- Calculator.java
+++ Calculator.java
@@ -1,20 +1,18 @@
 public class Calculator {
 
     public int add(int a, int b) {
+        System.out.println("Adding numbers");
         return a + b;
     }
 
     public int subtract(int a, int b) {
-        return a - b;
+        int result = a;
+        result = result - b;
+
+        return result;
     }
-}
+
     public int multiply(int a, int b) {
         return a * b;
     }
-
-    public int divide(int a, int b) {
-        if (b == 0) {
-            throw new IllegalArgumentException("Division by zero is not allowed.");
-        }
-        return a / b;
-    }
+}
