887c2f0ee9d769013d0610f6a8b8acadfc72fc38
T-V1 <teja.vajapeyazula@ascendion.com>
2026-01-19 00:19:24 +0530

    Update Sample.java

--- Sample.java
+++ Sample.java
@@ -1,1 +1,26 @@
+    public class SampleTestApp {
+
+        public static void main(String[] args) {
+            System.out.println("Application started");
+
+            int a = 10;
+            int b = 5;
+
+            int sum = add(a, b);
+            int product = multiply(a, b);
+
+            System.out.println("Sum: " + sum);
+            System.out.println("Product: " + product);
+
+            System.out.println("Application finished");
+        }
+
+        private static int add(int x, int y) {
+            return x + y;
+        }
+
+        private static int multiply(int x, int y) {
+            return x * y;
+        }
+    }
 
